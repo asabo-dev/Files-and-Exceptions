f46e53803c4cc53d6a09689bff2fcb7f2f747bd0
Efiom Ndaeyo <rylshadow@yahoo.com>
2025-02-28 00:05:19 +0800

    The strip() method removes whitespaces

--- file_reader.py
+++ file_reader.py
@@ -3,3 +3,8 @@
 with open("/Users/quanefiom/Desktop/Github_Projects/Chapter 10/pi_digits.txt") as file_object:
     contents = file_object.read()
     print(contents.rstrip())
+
+# Use the rstrip() method to remove the extra blank lines that appear in the output.
+# The rstrip() method removes any whitespace characters from the right side of a string.
+# The strip() method removes whitespace from both sides, and lstrip() removes whitespace from the left side.
+
