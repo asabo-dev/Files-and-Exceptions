b78fe5baceb7a23e7c42b8a55750e050d937c3d2
Efiom Ndaeyo <rylshadow@yahoo.com>
2025-02-27 23:53:19 +0800

    Reading the Content of a File

---
+++ file_reader.py
@@ -0,0 +1,5 @@
+# Reading the Content of a File
+
+with open("/Users/quanefiom/Desktop/Github_Projects/Chapter 10/pi_digits.txt") as file_object:
+    contents = file_object.read()
+    print(contents)
