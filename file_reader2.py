a726567eaaee379338b6b83a49292a68961eb0e0
Efiom Ndaeyo <rylshadow@yahoo.com>
2025-02-28 14:52:21 +0800

    Use rstrip() to remove whitespaces on the right

--- file_reader2.py
+++ file_reader2.py
@@ -2,4 +2,4 @@ filename = "/Users/quanefiom/Desktop/Github_Projects/Chapter 10/pi_digits.txt"
 
 with open(filename) as file_objects:
     for line in file_objects:
-        print(line.strip())
+        print(line.rstrip())
