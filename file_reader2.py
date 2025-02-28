3aa2066ee9afe07ef5afb006f7e95532d2eb7bd6
Efiom Ndaeyo <rylshadow@yahoo.com>
2025-02-28 14:54:58 +0800

    Update file_reader2.py

--- file_reader2.py
+++ file_reader2.py
@@ -1,5 +1,7 @@
 filename = "/Users/quanefiom/Desktop/Github_Projects/Chapter 10/pi_digits.txt"
 
 with open(filename) as file_objects:
-    for line in file_objects:
-        print(line.rstrip())
+    lines = file_objects.readlines()
+
+for line in lines:
+    print(line.rstrip())
