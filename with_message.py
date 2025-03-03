6e4134f157b051e76e6af3a814b4ca3732b17ed2
Efiom Ndaeyo <rylshadow@yahoo.com>
2025-03-03 11:20:32 +0800

    Add comment to code base.

--- with_message.py
+++ with_message.py
@@ -1,5 +1,8 @@
 filename = '/Users/quanefiom/desktop/github_projects/Chapter 10/programming.txt'
 
 with open(filename, 'w') as file_object:
+    """There are several parameters that can be passed to open()
+    such as: 'w' for write function, 'r' for read() which is default, 'a' for append().
+    """
     file_object.write("I love Python.")
 
