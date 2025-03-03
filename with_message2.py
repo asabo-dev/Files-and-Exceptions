6b1c18a15734b9d4f1992f529c8a9bfad9c131d7
Efiom Ndaeyo <rylshadow@yahoo.com>
2025-03-03 11:38:49 +0800

    Use append() to add text to an existing file

--- with_message2.py
+++ with_message2.py
@@ -1,11 +1,10 @@
-# Writing Multiple Lines
+# Use append() to Add Text to a File
 
 filename = '/Users/quanefiom/desktop/github_projects/Chapter 10/programming.txt'
 
-with open(filename, 'w') as file_object:
-    contents = "I love programming.\n"
-    contents += "I love creating new games.\n"
-    contents += "I also love working with data.\n"
+with open(filename, 'a') as file_object:
+    contents = "I also love finding meaning in large datasets.\n"
+    contents += "I love creating apps that can run in a browser.\n"
     file_object.write(contents)
     
 
