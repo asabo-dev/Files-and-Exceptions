225b900d6a9f7e8527cc3b7e48e9ff94f8a2d64d
Efiom Ndaeyo <rylshadow@yahoo.com>
2025-03-05 08:31:24 +0800

    Add Try-Except Block to alice.py to hanldle Error

--- alice.py
+++ alice.py
@@ -2,9 +2,9 @@
 
 filename = '/Users/quanefiom/desktop/github_projects/Chapter 10/alice.txt'
 
-with open(filename, encoding='utf-8') as f:
-    contents = f.read()
-"""
-This code is return an error because there no such file named 'alice.txt'.
-Use try-except Block to handle the FileNotFoundError gracefully.
-"""
+"""The try-except Block handles the FileNotFoundError gracefully."""
+try:
+    with open(filename, encoding='utf-8') as f:
+        contents = f.read()
+except FileNotFoundError:
+    print(f"Sorry, the file {filename} does not exist.")
