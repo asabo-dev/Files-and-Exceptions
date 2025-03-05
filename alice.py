afbc12bef4e49bd5c6e9bfe6a8f78cb1eeb0fcb6
Efiom Ndaeyo <rylshadow@yahoo.com>
2025-03-05 21:07:42 +0800

    Analyse the text in alice.txt

--- alice.py
+++ alice.py
@@ -1,10 +1,19 @@
-# Handling the FileNotFoundError Exception
+# Analyzing Text
 
 filename = '/Users/quanefiom/desktop/github_projects/Chapter 10/alice.txt'
 
-"""The try-except Block handles the FileNotFoundError gracefully."""
+"""The else block runs if the code in the try block runs successfully."""
 try:
     with open(filename, encoding='utf-8') as f:
         contents = f.read()
 except FileNotFoundError:
     print(f"Sorry, the file {filename} does not exist.")
+else:
+    # Count the approximate number of words in the file.
+    words = contents.split()
+    """
+    split() is a string method, which by default splits a string
+    wherever it finds any whitespace.
+    """
+    num_words = len(words)
+    print(f"The file 'alice.txt' has about {num_words} words.")
