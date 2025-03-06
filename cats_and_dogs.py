cefc5f648329e8723511533c52f470a2a2dd5c25
Efiom Ndaeyo <rylshadow@yahoo.com>
2025-03-06 23:18:03 +0800

    Fail silently, without reporting errror.

--- cats_and_dogs.py
+++ cats_and_dogs.py
@@ -1,6 +1,6 @@
-# EXERCISE 10-8: Cat and Dog names.
+# EXERCISE 10-9:  Silent Cat and Dog names.
 
-filename1 = '/Users/quanefiom/desktop/github_projects/Chapter 10/cats.txt'
+filename1 = ''
 filename2 = '/Users/quanefiom/desktop/github_projects/Chapter 10/dogs.txt'
 
 print("Cats: ")
@@ -8,11 +8,12 @@ try:
     with open(filename1, encoding='utf-8') as f:
         contents = f.read()
 except FileNotFoundError:
-    print("The file you are looking for cannot be found.")
+    pass
+    """Fail silently, with no report on the error."""
 else:
-    print(f"{contents}\n")
+    print(contents)
 
-
+print("\n")
 print("Dogs: ")
 try:
     with open(filename2, encoding='utf-8') as f:
